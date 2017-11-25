fe8896fc749510ad5747215e5482e732c3bc9aac
hecoding <hlaria@ucm.es>
2017-11-25 07:12:19 +0200

    Fix make_submission for log probs

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-import csv
 import time
+import os
+import pandas as pd
+import numpy as np
 
 def make_submission(predicted_labels, file_label='', log=False):
     """
@@ -10,13 +12,25 @@ def make_submission(predicted_labels, file_label='', log=False):
     file_label: str
         Optional string to label the resulting filename, in addition to the timestamp.
     """
-    if log:
-        predicted_labels = predicted_labels.astype(int)
     
-    if not file_label == '':
+    if log:
+        file_label += '_log'
+    
+    if file_label:
         file_label += '-'
     
-    with open('submissions/' + file_label + time.strftime("%Y-%m-%d-%H-%M-%S") + '.csv', 'w', newline='') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerow(['Sample_id', 'Sample_label'])
-        writer.writerows(enumerate(predicted_labels, start=1))
+    if log:
+        header = ['Sample_id','Class_1','Class_2','Class_3','Class_4','Class_5','Class_6','Class_7','Class_8','Class_9','Class_10']
+    else:
+        header = ['Sample_id', 'Sample_label']
+        assert all(predicted_labels % 1 == 0), 'Hey, you gave me a .something value! (integer or .0 assumed)'
+        predicted_labels = predicted_labels.astype(int)
+    
+    subm_dir = 'submissions'
+    if not os.path.exists(subm_dir):
+        os.makedirs(subm_dir)
+    
+    filename = subm_dir + '/' + file_label + time.strftime("%Y-%m-%d-%H-%M-%S") + '.csv'
+    df = pd.DataFrame(predicted_labels)
+    df.index += 1
+    df.to_csv(filename, index_label=header[0], header=header[1:])
