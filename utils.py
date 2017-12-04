e42d7f223c34e347043f2f8f8b2a1d03ca19bd64
hecoding <hlaria@ucm.es>
2017-12-04 19:55:37 +0200

    Confusion matrix plot

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import time
 import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import itertools
 
 def make_submission(predicted_labels, file_label='', log=False):
     """
@@ -34,3 +36,42 @@ def make_submission(predicted_labels, file_label='', log=False):
     df = pd.DataFrame(predicted_labels)
     df.index += 1
     df.to_csv(filename, index_label=header[0], header=header[1:])
+
+def plot_confusion_matrix(cm, classes=False,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not classes:
+        classes = range(1, cm.shape[0] + 1)
+    
+    plt.figure(figsize=(11,6))
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('./report_files/' + title.replace(" ", "_") + '.png', dpi=300, bbox_inches='tight');
